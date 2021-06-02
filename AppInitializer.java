ec8090ad22208567e369b153708e7948f48bd6aa
Dinusha Jayawardena <dinusha.g.jayawardena@gmail.com>
2021-06-02 12:15:22 +0530

    Added two new lines to AppInitializer.java file

--- AppInitializer.java
+++ AppInitializer.java
@@ -1,5 +1,7 @@
 public class AppInitializer{
     public static void main(String[] args){
+        system.out.println("--------------------");
         system.out.println("Hello Git Remotes");
+        system.out.println("--------------------");
     }
 }
